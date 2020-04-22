9d12f3940a60ae440c0bad5e9f6b494041dffc45
Andra Stroe <andrastroe1987@gmail.com>
2020-04-21 20:01:33 -0400

    Formatting

--- constants.py
+++ constants.py
@@ -12,19 +12,20 @@ from astropy import units as u
 from astropy.table import QTable
 from astropy.cosmology import FlatLambdaCDM
 
+
 @dataclass
 class Constants:
-    SN_limit: u.Quantity = 2 
-    H0: u.Quantity = 70 * u.km / (u.Mpc*u.s)
+    SN_limit: u.Quantity = 2
+    H0: u.Quantity = 70 * u.km / (u.Mpc * u.s)
     Om0: u.Quantity = 0.3
-    Tcmb0: u.Quantity = 2.725* u.K
+    Tcmb0: u.Quantity = 2.725 * u.K
     spectral_resolution: u.Quantity = 5
-    m_res: u.Quantity = 0.07445/u.Angstrom
-    c_res: u.Quantity  = 52.1895
-    tolerance: u.Quantity = 13.0*u.Angstrom
-    w: u.Quantity = 3*u.Angstrom
-    line_width: u.Quantity = 20*u.Angstrom
-    cont_width: u.Quantity = 70*u.Angstrom
+    m_res: u.Quantity = 0.07445 / u.Angstrom
+    c_res: u.Quantity = 52.1895
+    tolerance: u.Quantity = 13.0 * u.Angstrom
+    w: u.Quantity = 3 * u.Angstrom
+    line_width: u.Quantity = 20 * u.Angstrom
+    cont_width: u.Quantity = 70 * u.Angstrom
     fwhm_min: u.Quantity = 2.0
     fwhm_max: u.Quantity = 15.0
 
@@ -32,10 +33,7 @@ class Constants:
 def read_config(config_file) -> Constants:
     config = yaml.safe_load(open(config_file).read())
     print(config)
-    data = {
-        key: u.Quantity(value)
-        for key, value in config.items()
-        }
+    data = {key: u.Quantity(value) for key, value in config.items()}
     return Constants(**data)
 
 a = read_config('constants.param')
