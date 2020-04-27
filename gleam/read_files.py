f4b57b80488b4751da44e6c1b327de4648d54688
Andra Stroe <andrastroe1987@gmail.com>
2020-04-27 17:29:02 -0400

    Read master file from ascii or fits into QTable

--- gleam/read_files.py
+++ gleam/read_files.py
@@ -2,71 +2,39 @@ __author__ = "Andra Stroe"
 __version__ = "0.1"
 
 import glob
+import sys
 
 import numpy as np
 from astropy.io import fits
 from astropy.table import QTable, Column
 from astropy import units as u
+from colorama import Fore
 
 import gleam.constants as c
 
 
 def read_lof(file1):
-    """
-    For each sample, quadrants and extensions, it reads the head file produced 
-    by specpro, which contains a list of the sources and their properties
+    """For each sample, telescope setup and pointing, it reads the master file, 
+    which contains a list of the sources and their properties.
     Input:
-        data_path: folder location of head file and all the individual spectra
-                   ! assumes the name contains "_zinfo.dat" in order to find it
-                   and that there is only 1 such file in each folder
+        file1: master file in ascii or fits format
         The format of the head file is the following
-        #Setup Pointing SourceNumber RA DEC Sample Redshift Confidence Crew Comments
+        # Setup Pointing SourceNumber RA DEC Sample Redshift Confidence Initials Membership   Type
     Return: 
         Astropy Table with measurements of interest: the source number, RA, DEC,
-        the parent sample, redshift (from specpro) and the z confidence
+        the parent sample, redshift (from specpro) and the z confidence. Throws
+        error if the file is not of the right type.
     """
-    data = np.array(np.genfromtxt(file1, dtype="U8,U20,i,f,f,U10,f,f,U2,U20,U20"))
-
-    # Extract each measurement of interest into a separate Table
-    t = QTable()
-    t["Setup"] = Column(
-        [datum[0] for datum in data], dtype="U", description="Telescope setup",
-    )
-    t["Pointing"] = Column(
-        [datum[1] for datum in data],
-        dtype="U",
-        description="Stack type or telescope source",
-    )
-    t["SourceNumber"] = Column(
-        [datum[2] for datum in data], dtype="U", description="SourceNumber"
-    )
-    t["RA"] = Column(
-        [datum[3] for datum in data],
-        unit=u.degree,
-        dtype="f",
-        description="Right Ascension",
-    )
-    t["DEC"] = Column(
-        [datum[4] for datum in data],
-        unit=u.degree,
-        dtype="f",
-        description="Declination",
-    )
-    t["Sample"] = Column(
-        [datum[5] for datum in data], dtype="U", description="Parent sample"
-    )
-    t["Redshift"] = Column(
-        [datum[6] for datum in data], dtype="f", description="Redshift"
-    )
-    t["Confidence"] = Column(
-        [datum[7] for datum in data],
-        dtype="f",
-        description="Confidence as assigned by user. "
-        "Scales from 1 (best) to 5 (worst)",
-    )
-    t["Membership"] = Column([datum[9] for datum in data], dtype="U20")
-    t["Type"] = Column([datum[10] for datum in data], dtype="U20", description="Type")
-    return t
+    try:
+        table = QTable.read(file1, format="fits")
+        return table
+    except:
+        try:
+            table = QTable.read(file1, format="ascii.commented_header")
+            return table
+        except:
+            print(Fore.RED + "Cannot find master redshift file")
+            sys.exit("Error!")
 
 
 def naming_convention(data_path, sample, source_number, setup, pointing, mod):
